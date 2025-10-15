a74f1555ce300e1dd0008fc2c673049bd2a999e6
Sadegh Movahed <sadeqmail136@gmail.com>
2025-10-16 01:49:13 +0330

    fix build errors

--- src/app/dashboard/ClientActions.tsx
+++ src/app/dashboard/ClientActions.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cookies } from "next/headers";
 import ClientActions from "./ClientActions";
 import styles from './dashboard.module.scss'
